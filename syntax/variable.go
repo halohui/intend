d4c664965a1773039f830b4dd40e6d67b36902d3
halohui <xiaohui.sysu@gmail.com>
2018-01-04 15:23:11 +0800

    modification

--- syntax/variable.go
+++ syntax/variable.go
@@ -4,13 +4,19 @@ import "fmt"
 
 //声明主要有变量，常量，类型以及函数
 
-func VariableDeclare(){
-	fmt.Println(f()==f()) //false
+func VariableDeclare() {
+	fmt.Println(f() == f()) //false
 }
 
-func f()*int{
-	v:=1
-	return &v  //可以返回局部变量的指针，Go会做逃逸分析
+func f() *int {
+	v := 1
+	return &v //可以返回局部变量的指针，Go会做逃逸分析
 }
 
-
+func fib(n int) int {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		x, y = y, x+y  //使用并行赋值来求菲波那契数列
+	}
+	return x
+}
